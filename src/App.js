ed8731e8945a91bd8db3ce106d1b051c9b8b6312
imusun <imusun190812@gmail.com>
2022-03-09 18:37:10 +0900

    진짜 모르겠다 나는

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          아 하기싫어ㅋㅋ
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          십ㅋㅋ
         </a>
       </header>
     </div>
