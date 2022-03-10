d8fc38300305511efc3a515ea7946a9bf9114062
imusun <imusun190812@gmail.com>
2022-03-10 13:40:17 +0900

    ~#6

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -4,6 +4,18 @@ import styles from "./Hello.module.css"
 export default function Hello() {
     return <div>
         <h1>Hello</h1>
+
+        <button onClick={() => {
+            alert("코로나");
+        }}>Show name</button>
+        
+        <div>
+        <input onChange={(txt) => {
+            console.log(txt.target.value);
+            }
+        } />
+        </div>
+
         <div className={styles.box}>Hello</div>
         <World />
     </div> 
