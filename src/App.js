6d3d5e0dca71401a74e7ff2ff7bcb984c329b5b9
imusun <imusun190812@gmail.com>
2022-03-10 15:11:16 +0900

    ~#8

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import Welcome from './component/Welcome';
 function App() {
   return (
     <div className="App">
-
-      <Hello />
+     
+      {/* jsx에서는 함수를 만들어 import하여 태그처럼 쓴다. */}    
+      <Hello age = {10} /> {/* props를 넘겨줌 */}
       <div className="box"> <h2>#8A2BE2</h2> </div>   
-      
+
     </div>
   );
 }
--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,11 +1,14 @@
 import World from "./World"
 import styles from "./Hello.module.css"
 import { useState } from "react"
+import UserName from "./UserName";
 
-export default function Hello() {
+export default function Hello(props) {
     const [name, setName] = useState("코로나");
+    const [age, setAge] = useState(props.age);
 
     return <div>
+ 
         <h1>React Study</h1>
         
         <div className={styles.box}>purple</div>
@@ -22,15 +25,17 @@ export default function Hello() {
         </div>
 
         <div>
-            <h2 id = "name"> {name} </h2>
+            <h2 id = "name"> {name} : {age} </h2>
         <button onClick={
             () => {
                 setName(name === "코로나" ? "확진" : "코로나");
+                setAge(age + 1);
             }
         }> 
         ㅋㅋ
         </button>
-
+        {/* 여기서의 name은 state, username에선 props */}
+        <UserName name = {name}/>
         </div>
 
     </div> 
